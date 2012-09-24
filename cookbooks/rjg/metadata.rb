17decdcf52559e71743e730fffd6b4cc19bf78bd
rgeyer <ryan.geyer@rightscale.com>
2012-09-24 20:47:37 +0000

    Added a hello world recipe

--- cookbooks/rjg/metadata.rb
+++ cookbooks/rjg/metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Ryan J. Geyer"
+maintainer_email "me@ryangeyer.com"
 license          "All rights reserved"
 description      "Installs/Configures rjg"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+recipe "rjg::helloworld", "Prints hello world"
