33fd5df4a71d124350cde5afc1198ae44f7467e3
rgeyer <ryan.geyer@rightscale.com>
2012-09-25 23:48:34 +0000

    Add rightscale_marker to helloworld

--- cookbooks/rjg/recipes/helloworld.rb
+++ cookbooks/rjg/recipes/helloworld.rb
@@ -1,5 +1,9 @@
+rightscale_marker :begin
+
 log "Hello World"
 
 log "The package that would be installed is #{node['rjg']['apache_package_name']}"
 
 package node['rjg']['apache_package_name'] 
+
+rightscale_marker :end
