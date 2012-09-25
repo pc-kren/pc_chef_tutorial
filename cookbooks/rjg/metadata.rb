38b6d9655b615d3b3931c4e7f4afdd37c6188692
rgeyer <ryan.geyer@rightscale.com>
2012-09-25 23:24:32 +0000

    Added rightscale_marker

--- cookbooks/rjg/metadata.rb
+++ cookbooks/rjg/metadata.rb
@@ -5,6 +5,10 @@ description      "Installs/Configures rjg"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+%w{rightscale}.each do |d|
+  depends d
+end
+
 recipe "rjg::helloworld", "Prints hello world"
 recipe "rjg::install_foo", "Moves foo.txt to /tmp/foo.txt"
 recipe "rjg::change_index", "Changes the /var/www/index.html content"
--- cookbooks/rjg/recipes/change_index.rb
+++ cookbooks/rjg/recipes/change_index.rb
@@ -1,4 +1,8 @@
+rightscale_marker :begin
+
 template "/var/www/index.html" do
   source "index.html.erb"
   variables :name => node['rjg']['my_name']
 end
+
+rightscale_marker :end
